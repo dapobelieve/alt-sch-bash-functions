8fbcef8bd1e6dd294f038cfabeeafd7dce0fcb6c
Dafe Avwerosuoghene <dafe_crucifix@yahoo.com>
2024-11-28 15:23:32 +0100

    initial commit

---
+++ assignment_1.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+# Define the function
+get_fullname () {
+    echo -e "\nFullname: Dafe Avwerosuoghene"
+}
+print_bio() {
+    echo -e "\nBio: I'm a highly motivated and enthusiastic individual with a passion for technology and innovation" 
+    }
+print_year_in_tech () {
+    echo -e "\nNumber of years in tech: 4"
+}
+print_dream_job() {
+    echo -e "\nDream job: Cloud engineer"
+}
+print_goals() {
+    echo -e "\nGoals:"
+    echo "- learn new skills"
+    echo "- Build innovative project"
+    echo "- Contribute to open-source communities."
+}
+
+# Call the functions
+get_fullname
+print_bio
+print_year_in_tech
+print_dream_job
+print_goals
