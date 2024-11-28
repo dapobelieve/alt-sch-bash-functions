c5abcacd13d622ca205925d026545f01be848b2f
Rodriguekmr <kmrodrigue7@gmail.com>
2024-11-29 00:05:18 +0100

    answers to the assignment-1

---
+++ rodrigue.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+who_am_I() {
+    echo "Hello, I am a cloud student.🐱‍🚀"
+}
+
+full_name() {
+    echo "Rodrigue K.M."
+}
+
+bio() {
+    echo "Bio: I am a passionate architect with a keen 
+    interest in machine learning and artificial intelligence."
+}
+
+years_in_tech() {
+    echo "Years in Tech: 0.5"
+}
+
+dream_job() {
+    echo "Dream Job: Senior AI Researcher"
+}
+
+goals_⚽() {
+    echo "Goals ⚽: "
+    echo "1. Build a scalable AI model"
+    echo "2. Contribute to open-source projects"
+    echo "3. Get certified in cloud"
+    echo "4. Attend a global tech conference"
+}
+
+
+who_am_I
+full_name
+bio
+years_in_tech
+dream_job
+goals_⚽
