ce3ead93db811c9095f28ef37cc43282a9bfbd25
Perez <perescrypto7@gmail.com>
2024-11-30 16:42:59 +0100

    My Assignment script commit

---
+++ perez.sh
@@ -0,0 +1,39 @@
+# write bash fucntions that prints out the following infomation about you
+# Fullname
+# Bio
+# Year in tech
+# Dream job
+# Goals ⚽
+
+Fullname() {
+	echo "My full name is Perez Osunu"
+}
+
+Bio() {
+	echo "I am from a humble background seeking to gain kn owledge in tech and build a fortue for myself and my family. I love people and I love nature"
+}
+
+Year-in-teach() {
+	echo "I started learning tech in late 2023, but only taking it serious in this 2024"
+}
+
+Dream-job() {
+
+	echo "My dream job is to become self-emplyed with viable solutions that would make an incredible impact in the lives of millions around the world"
+}
+
+
+Goals() {
+	echo "1. I intend to finish altschool strong"
+	echo "2. My goal is to be relevant to the society using the tech means"
+	echo "3. My goal is to make as much money as possible and help as many people as possible"
+	echo "4. To be relevant to God and live for him in the whole of my life"
+}
+
+#Call the functions
+
+Fullname
+Bio
+Year-in-tech
+Dream-job
+Goals
