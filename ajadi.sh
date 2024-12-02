cc57e83d1474f4f0b9903ba651259a6f48628f13
Ajadi <ajadiadedeji@gmail.com>
2024-12-02 02:18:48 +0100

    Removed Instructor file

--- ajadi.sh
+++ ajadi.sh
@@ -12,6 +12,43 @@ fullname() {
 	echo "My name is $1 $2 $3"
 }
 
-fullname Ajadi Quadri Adedeji
+Bio() {
+	cat << EOF
+$(fullname)Ajadi Quadri Adedeji
+
+# I had my first degree in mechanical engineering from FUTA.
+# I currently work as a maintenance specialist with a glass manufacturing company.
+
+# Over the years, I developed interest in tech and have been doing some self learning on data analysis.
+# Along the line, a friend recommended Altschool Africa to me,
+
+# Funny enough, after going through all the courses available; I decided to go for the cloud program.
+# It's been fun all the while.
+EOF
+}
+
+Year_in_tech() {
+	echo "I have about 2 years experience in tech"
+}
+
+Dream_job() {
+	echo "I will like to work as a Senior Devops engineer for a cloud giant"
+}
+
+Goals() {
+	cat << EOF
+# My goals are
+# To become a senior devops engineer within the next 3 years
+# To be Healthy
+# Happy
+# Wealthy
+EOF
+}
+
+
+Bio
+Year_in_tech
+Dream_job
+Goals
 
 # Nice having you around
