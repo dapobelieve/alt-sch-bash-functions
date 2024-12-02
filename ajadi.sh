54938e3f5a7a1c8b2548bca9c6d107171273ec19
Ajadi <ajadiadedeji@gmail.com>
2024-12-02 01:50:54 +0100

    Added ajadi.sh

---
+++ ajadi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Welcome to my space
+# You will learn about 
+# my name
+# Bio
+# year in tech
+# Dream job
+# Goals
+# Let's get started
+
+fullname() {
+	echo "My name is $1 $2 $3"
+}
+
+fullname Ajadi Quadri Adedeji
+
+# Nice having you around
