8392eecd995590a6ac785fb39df692df411dbc20
Justin Omeke Chukwuemeka <almightydeus65@gmail.com>
2024-12-03 10:30:29 +0000

    First bash assignment commit

---
+++ justin.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+greet_user (){
+  echo "Hello, $1! Welcome to Justin's first bash scripting"
+}
+
+Fullname (){
+  echo "My name is Justin Omeke Chukwuemeka."
+}
+
+Bio (){
+  echo "My name is Justin Omeke Chukwuemeka and I am from Enugu State. A recent graduate of Enugu State Polytechnic, I earned a Bachelor's degree in Computer Science. As an aspiring Cloud Engineer at AltSchool Africa, embracing the challenges and reward of this new field, which I am determined to suceed in this field because my passion for learning and resilience drives me to overcome obstacles and adapt to new environments. My interests entend beyond academics. I am deeply passionate about technology.
+      My dedication for self-improvement is also evident in my aspiration to attend culinary school and explore my creative side through that aligns with my love for tech. I’m excited to continue learning and growing in the field of Computer Science at AltSchool. My goal is to become a cloud engineer at a leading web3 tech company where I can contribute to developing innovative solutions that make a positive impact on society and free everyone from government restrictions."
+}
+
+DreamJob (){
+  echo "My dream job is to be a Cloud Engineer"
+}
+
+
+YearInTech (){
+  echo "During my IT period in tertiary school days was when I first experienced HTML, CSS and Javascript and that should be in 2021. So I have been in the Tech space for over 3+ years."
+}
+
+Goals (){
+  echo "Goal1: Practical experience is crucial for understanding the nuances of working in tech and for building your professional network
+
+        Goal2: Excel in Cloud Engineering: Gain a deep understanding of cloud Tech and establish a successful career in tecnology.
+        
+        Goal3: Actively participate in open source projects to gain experience and contribute to the tech community."
+}
+
+Hobbies (){
+  echo "Listening to Music: I love listening to music, i find solace in music particularly R&B, It's resonates deeply with me. Its soothing melodies and heartfelt lyrics provide me witha a sense of calm and comfort, creating a peaceful escape from the demands of daily life.
+  
+        Playing Basketball: Back then I love playing basketball and I still have strong interest in it."
