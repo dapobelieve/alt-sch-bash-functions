40a371472eaddfd7065cf31adc71fa81e9ecc2c7
Micaiah Obinwa <micaiahobinwa4data@gmail.com>
2024-12-03 17:26:13 +0100

    Added Jerry.sh script

---
+++ Jerry.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Instructor's example
+who_am_i() {
+  echo "Hello, I am Jerry Ovoh"
+}
+
+# Function to display your full name
+fullname() {
+  echo "Jerry Ajirioghene Ovo"
+}
+
+# Function to display your bio
+bio() {
+  echo "I am a graduate of Electrical/ Electronics Engineering of the University of Benin, I graduated with a Second Class Upper Division and have worked in the Telecoms industry. I am certified in AWS and currently studying Cloud Engineering in Altschool."
+}
+
+# Function to display your years in tech
+years_in_tech() {
+  echo "About 8 years in Tech"
+}
+
+# Function to display your dream job
+dream_job() {
+  echo "To be a world-class Cloud Engineer"
+}
+
+# Function to display your goals
+goals() {
+  echo "To excel in my chosen field of studies and contribute my quota towards the growth of Cloud Engineering."
+}
+
+# Calling the functions
+who_am_i
+fullname
+bio
+years_in_tech
+dream_job
+goals
