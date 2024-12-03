b88a84e325c0cf9c67baa3300e34c02cf7516ea4
Uzoagwa Bethel <uzoagwabethel@gmail.com>
2024-12-03 09:38:28 +0100

    first commit

---
+++ uzoagwa bethel boniface.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Function that will print out my full name
+full_name() {
+  echo "Full Name: Uzoagwa Bethel Boniface"
+}
+
+# Function that will print out my bio
+bio() {
+  echo "Bio: i am a tecnical operator with the Nigerian Bottling Company ltd. Also am student
+   of Altschool Africa where i am currently studying cloud engineering technology where i will 
+   become a passionate software developer, exploring the world of technology, coding, networking 
+   and innovation. Enthusiast of open-source contributions and constantly learning new tools to 
+   build great solutions where people can find help and get answers to their question."
+}
+
+# Function that will print out the years in tech
+years_in_tech() {
+  echo "Years in Tech: i am currently six months into tech"
+}
+
+# Function that will print out my dream job
+dream_job() {
+  echo "Dream Job: to be a cloud engineer at a tech company, working on innovative platforms like 
+        AWS, Azure, and Google Cloud to build scalable and secure cloud environments."
+}
+
+# Function that will print out my goals
+goals() {
+  echo "Goals ⚽:"
+  echo "- Master Cloud Platforms such as (AWS, Azure, GCP)
+          Become proficient in the core services of your chosen platform. Learn to work with compute 
+          services (like EC2 or Virtual Machines), storage (like S3 or Blob Storage), databases,
+          networking, and security."
+  echo "- Contribute to open-source projects"
+  echo "- Learn cloud computing and serverless architectures"
+  echo "- Build scalable applications and make a positive impact with technology"
+  echo "- Automate Infrastructure with Infrastructure as Code (IaC)  Learn IaC tools such as Terraform, 
+          AWS CloudFormation, or Azure Resource Manager (ARM) templates to automate cloud infrastructure."
+}
