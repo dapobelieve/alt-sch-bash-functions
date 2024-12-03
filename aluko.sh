7569c3f2cdd1e426b0ebc30c9b600bfa8a4048db
Aluko Oluwadamilola <87522676+damzking@users.noreply.github.com>
2024-12-03 09:01:43 +0000

    aluko.sh

---
+++ aluko.sh
@@ -0,0 +1,18 @@
+who_am_i() {
+    echo "My name is Aluko Oluwadamilola"
+}
+
+bio() {
+     echo "I am an aspiring cloud engineer"
+}
+goals() {
+    echo "My main goal is to develop and deploy cloud-native applications"
+}
+
+years_in_tech() {
+    echo "I have been working in the tech industry for 3 years"
+}
+
+experience() {
+    echo "As a cloud engineer, I have worked with various cloud providers such as AWS, Azure, and Google Cloud Platform"
+}
