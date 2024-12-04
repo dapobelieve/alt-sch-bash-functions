49e9cb530e2ebbbaecfc06bb24ce2c8b10d144c0
tomiwa <aturutommy@gmail.com>
2024-12-04 17:31:13 +0100

    assignment

---
+++ Tomiwa.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+print_fullname() {
+    echo "Full Name: Aturu Oluwatomiwa Miracle"
+}
+
+print_bio() {
+    echo "Bio: Christian, Electrical Engineering student in UNILAG"
+}
+
+print_year_in_tech() {
+    echo "Year in Tech: Second year in tech"
+}
+
+print_dream_job() {
+    echo "Dream Job: DevOps engineer at a startup"
+}
+
+print_goals() {
+    echo "Goals: To become one of the leading figures in the profession and to lead others effectively into the same goals"
+}
+
+print_fullname
+print_bio
+print_year_in_tech
+print_dream_job
+print_goals
