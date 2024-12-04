d7617d00cfa96e7d0394442443fd42048c98b60b
Akor Raymond <akorilemona20@gmail.com>
2024-12-04 14:23:08 +0100

    git and bash assingment

---
+++ akor.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/bash
+
+full_name() {
+    echo "Akor Raymond Ilemona"
+}
+
+bio() {
+    echo "Country of Origin: Nigeria"
+    echo "State of Origin: Kogi"
+    echo "State of Residence: Kano"
+    echo "Current Job: Medical Sales Representative"
+    echo "Organization: DKT-Nigeria"
+    echo "Relationship Status: Married"
+    echo "Education: Bsc. Biochemistry"
+    echo "About: Highly motivated and detail-oriented tech enthusiast with months of experience in learning and exploring various technologies. Proven ability to quickly absorb new concepts, collaborate with others, and apply technical skills to real-world problems. Proficient in HTML, Intermidiate in CSS, Java script programming languages and currently studying Cloud Engineering. Eager to leverage skills and knowledge to drive innovation and growth in the tech industry."
+}
+
+year_in_tech() {
+    echo "Tech Experience: About a year"
+}
+
+dream_job() {
+    echo "Cloud Architect & Engineer"
+    echo "Being able to design, build and maintain scalable, secure, and efficient cloud infrastructure and applications.  And also collaborate with cross-functional teams to drive innovation and deliver high-quality solutions"
+}
+
+goals() {
+    echo "CAREER GOALS:
+1. Design and implement scalable cloud architectures for high-traffic applications
+2. Develop expertise in cloud security, compliance, and governance
+3. Automate cloud resource provisioning using DevOps tools
+4. Optimize cloud costs through efficient resource utilization
+5. Collaborate with cross-functional teams to drive innovation."
+    echo "TECHNICAL GOALS:
+1. Master cloud platforms: AWS, Azure, Google Cloud
+2. Develop skills in containerization (Docker) and orchestration (Kubernetes)
+3. Learn programming languages: Python, Java, JavaScript
+4. Familiarize yourself with DevOps tools: Jenkins, GitLab CI/CD
+5. Explore emerging technologies: Serverless computing, cloud-native applications."
+    echo "LONG-TERM GOALS:
+1. Lead cloud engineering teams and drive strategic decisions
+2. Architect complex cloud systems for large-scale enterprises
+3. Develop thought leadership in cloud computing through blogging, speaking, or research
+4. Create innovative cloud solutions that drive business growth
+5. Pursue leadership roles in cloud computing companies."
+    echo "PERSONAL GOALS:
+1. Stay updated with industry trends and best practices
+2. Network with cloud professionals through conferences and meetups
+3. Pursue cloud certifications: AWS Certified Cloud Practitioner, Azure Certified Solutions Architect
+4. Develop soft skills: communication, collaboration, problem-solving
+5. Mentor junior engineers and share knowledge
+6. MAKES MY MAMA AND MY COUNTRY NIGERIA PROUD."
+}
+
+
+full_name
+bio
+year_in_tech
+dream_job
+goals
