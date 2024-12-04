dbbb3b3b73717e2fb2dbc754918e7aa03ae4f832
vetoxyl <vetoxyl63@gmail.com>
2024-12-04 20:56:54 +0100

    abiola_assignment

---
+++ abiola_assignment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+who_am_i() {
+  echo "Hello, I am the Cloud Instructo🎉"
+}
+
+fullname() {
+  echo "Fullname: Okunola Abiola"
+}
+
+bio() {
+  echo "Bio: I am a results-driven Cloud Engineer with expertise in designing, implementing, and managing scalable, secure, and cost-effective cloud solutions. With a strong foundation in cloud platforms like AWS, Azure, and Google Cloud, I specialize in building automated workflows, deploying infrastructure as code, and optimizing cloud environments for performance and reliability"
+}
+
+year_in_tech() {
+  echo "I have been in tech for five year"
+}
+
+dream_job() {
+  echo "Dream Job: To be an AWS cloud/devops Engineer"
+}
+
+goals() {
+  echo "Goals ⚽: Design and implement monitoring and logging solutions using AWS tools like CloudWatch, X-Ray, or third-party integrations. Ensure high availability, fault tolerance, and cost optimization of cloud-based applications and infrastructure."
+}
