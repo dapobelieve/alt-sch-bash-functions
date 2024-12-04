b0d4276e9d4c128a6f4215601112e9177f0ee4bb
Izuchukwu Nwachukwu <nwachukwualexiz@gmail.com>
2024-12-04 10:58:25 +0100

    Izuchukwu Assignment

---
+++ izuchukwu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+who_am_i() {
+  echo "Hello, I am a Cloud Engineering student"
+}
+
+Fullname() {
+  echo "My name is Izuchukwu Nwachukwu"
+}
+
+Bio() {
+  echo "I am a Nigerian currently pursuing a Diploma in Cloud Engineering at AltSchool Africa. My Academic background is in Economics while profesionaly, I have accumulated substantial experience in the Humanitarian sector."
+}
+
+Year_in_tech() {
+  echo "I've recently hit the 6-month milestone in my tech journey"
+}
+
+Dream_job() {
+  echo "DevOps Engineer is my ultimate career goal"
+}
+
+Goals() {
+  echo "To leverage my skills to streamline processes and improve efficiency"
+}
+
