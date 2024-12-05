21a1837c1e28743bf126d58ea1543af7bb198b31
peterukpabi <peterukpabi4@gmail.com>
2024-12-05 19:17:54 +0000

    updated my commands to use functions instead just echo commands

--- UkpabiPeter-Script.sh
+++ UkpabiPeter-Script.sh
@@ -1,19 +1,35 @@
+
 #!/bin/bash
 
+# Function to introduce yourself
+My_name() {
+    echo "My Name is Ukpabi Peter Uchenna"
+}
 
+# Function to describe your background
+Bio () {
+    echo "I am from Arochukwu Local Government, Abia State, Nigeria. I am a student of Altschool Africa, School of Engineering (Cloud Computing) track"
+}
 
+# Function to state when you got into tech
+state_tech_start() {
+    echo "I got into Tech in the year 2024"
+}
 
+# Function to share your dream job
+My_dream_job() {
+    echo "My dream job is to work as a Senior DevOps Engineer in an organization"
+}
 
-echo "My Name is Ukpabi Peter Uchenna"
+# Function to share your goal
+share_goal() {
+    echo "My goal is to graduate from Altschool as one of the best graduating students, filled with so much knowledge and also become a problem solver"
+}
 
-echo "I am from Arochukwu Local Government, Abia State, Nigeria. I am a student of Altschool Africa, School of Engineering(Cloud Computing)track
-"
-
-echo "I got into Tech in the year 2024"
-
-
-
-echo "My dream job is to work as a Senior Devops Engineer in an Organization"
-
-echo "My goal is to graduate from Altschool as one of the best graduating students, filled with so many knowledge and also become a problem solver"
+# Main script execution
+introduce_yourself
+describe_background
+state_tech_start
+share_dream_job
+share_goal
 
