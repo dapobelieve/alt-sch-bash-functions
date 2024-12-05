367b365da21bd6c4b93b4f75566124e4430c243e
Udokz <kierianudoka@gmail.com>
2024-12-05 21:10:01 +0100

    My Asignment1

---
+++ Kierian_Udoka.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+who_am_i() {
+    echo "Hello My name is Mgbehota Kierian Udoka"
+}
+
+
+Bio() {
+    echo "Hi my name is MGBEHOTA KIERIAN UDOKA i am a student of Alt School Africa School Of Engineeering Karatu 24 set and also a working class citizen of Nigeria. I graduated 
+          from Nnamdi Azikiwe University Awka, Anambra state Nigeria in 2019 and i got into tech in december 2022 where i started off with graphic design but now i hope
+          to become a certified cloud engineer in the coming years"
+}
+
+Years_in_tech() {
+    echo "I have been in the tech space for 2 years"
+}
+
+
+
+Dreamjob()  {
+    echo "A Cloud Solutions Architect and a tech founder"
+}
+
+
+
+
+Goals() {
+   echo "To become a certified cloud engineer"
+   echo "Have my own business"
+   echo "Travel the world"
+   echo "Stay healthy and fit"
+}
+
+
+who_am_i
+#Bio
+Years_in_tech
+Dreamjob
+Goals
