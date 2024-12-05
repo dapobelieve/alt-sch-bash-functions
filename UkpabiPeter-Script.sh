9de872373d91df096aeac3f9719f35f972b1d385
peterukpabi <peterukpabi4@gmail.com>
2024-12-05 18:25:24 +0000

    added my first commit to UkpabiPeter-branch(assignment)

---
+++ UkpabiPeter-Script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+
+
+
+echo "My Name is Ukpabi Peter Uchenna"
+
+echo "I am from Arochukwu Local Government, Abia State, Nigeria. I am a student of Altschool Africa, School of Engineering(Cloud Computing)track
+"
+
+echo "I got into Tech in the year 2024"
+
+
+
+echo "My dream job is to work as a Senior Devops Engineer in an Organization"
+
+echo "My goal is to graduate from Altschool as one of the best graduating students, filled with so many knowledge and also become a problem solver"
+
