2bf7e4eff2e5db6799b5d73df9f7cc5e3abe6444
Izudavo <izuchukwudavido@gmail.com>
2024-12-05 01:33:40 +0100

    Fix line endings in bio script

---
+++ davidobinta.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+who_am_i() {
+    echo "Hello, my name is David Obinta"
+}
+
+Bio() { 
+    echo "I am in the cloud engineering track with reg no: ALT/SOE/024/0930"
+    echo "I am 20years old, currently a student of FUTA, disciple Biomedical Technology"
+    echo "I basically seek Solutions the drive to impossible goals making them possible" 
+
+AWS_certification() {
+    echo "I recently earned the AWS Certified Cloud Practitioner (CLF-C02) certification."
+    echo "I'm now preparing for the AWS Certified Solutions Architect – Associate (SAA-C03) exam."
+}
+
+Dream_job() {
+    echo "I would like to work as a DevOps engineer at Nvidia."
+}
+
+Goals() {
+    echo "I would love to become a professional in my field."
+    echo "To incorporate Web3 into my tech life."
+    echo "To develop tech solutions that would help the country."
+    echo "To improve on my programming languages."
+    echo "To pursue advanced AWS certifications, including AWS Certified DevOps Engineer – Professional."
+    echo "To master Terraform and DevOps automation tools."
+}
+
+who_am_i 
+echo
+
+Bio 
+echo
+
+Years_in_tech 
+echo
+
+AWS_certification
+echo
+
+Dream_job 
+echo
+
+Goals 
+
