c7d5d8a592f65070d3a48661bc9bfc948cd0498a
TaiwoOlatunde1 <olatunde.taiwo1111@gmail.com>
2024-12-06 11:43:41 +0100

    commit for taiwo.sh

---
+++ taiwo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+full_name() {
+    echo "Full Name: Taiwo Olatunde"
+}
+full_name
+
+bio() {
+    echo -e "\nBio: I am a graduate in Physics and Electronics from Adekunle Ajasin University, and I am currently undergoing my service year."
+}
+bio
+
+year_in_tech() {
+    echo -e "\nYear in Tech.: I have 3 years of experience in the tech industry, where I have developed skills in web development and cloud engineering."
+}
+year_in_tech
+
+dream_job() {
+    echo -e "\nDream Job: My dream job is to work as a Software Developer and Cloud Engineer, where I can leverage my technical skills to create innovative software solutions and optimize cloud infrastructure."
+}
+dream_job
+
+goals() {
+    echo -e "\nGoal 1: My goal is to become a successful professional in the tech industry, continuously growing my skills and contributing to innovative solutions that drive progress."
+    echo -e "\nGoal 2: My goal is to address the world's problems through technological innovations."
+    echo -e "\nGoal 3: My goal is to become one of the most successful people on Earth." 
+}
+goals
+
+hobby() {
+    echo -e "\nHobby: My hobby is playing football, which helps me stay active and develop teamwork skills."
+}
+hobby
