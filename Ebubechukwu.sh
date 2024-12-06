259f8203ef97a936a67b4ea21eaba2d05c76045f
Ebube101 <ogubunkaebube@gmail.com>
2024-12-06 14:04:24 -0800

    this is my assignment delivered

---
+++ Ebubechukwu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+who_am_i() {
+  echo "Hello, I am not the Cloud Instructor 🎉, I am the cloud instructor student"
+}
+
+fullname() {
+  echo "Full Name: Ebubechukwu Ogubunka"
+}
+
+bio() {
+  echo "Bio: I am a passionate tech enthusiast with a focus on cloud engineering and cybersecurity."
+}
+
+year_in_tech() {
+  echo "Years in Tech: 3 years"
+}
+
+dream_job() {
+  echo "Dream Job: DevSecOps Engineer"
+}
+
+goals() {
+  echo "Goals ⚽: 
+  1. Earn AWS Certified Solutions Architect Certification.
+  2. Contribute to impactful open-source projects.
+  3. Build innovative cloud-based solutions."
+}
+
+
+who_am_i
+fullname
+bio
+year_in_tech
+dream_job
+goals
