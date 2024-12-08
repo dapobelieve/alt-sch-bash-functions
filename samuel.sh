9b69c544db1ee9b850f74e8cd643823537f4594d
Per-Tec <samuelitiola.uk@gmail.com>
2024-12-08 20:56:20 +0000

    updated assignment

---
+++ samuel.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+who_am_i() {
+  echo "Hello, I am the Cloud Instructo🎉"
+}
+
+fullname() {
+  echo "Fullname: Samuel Itiola"
+}
+
+bio() {
+  echo "Bio: GIS expert transitioning into cloud engineering, passionate about spatial data, automation, and technology-driven solutions."
+}
+
+year_in_tech() {
+  echo "Year in tech: 5+ years of experience in GIS and geospatial technologies."
+}
+
+dream_job() {
+  echo "Dream Job: Cloud Solutions Architect specializing in geospatial technologies."
+}
+
+goals() {
+  echo "Goals ⚽: 
+1. Achieve expertise in cloud engineering.
+2. Develop innovative geospatial solutions using cloud platforms.
+3. Earn advanced certifications in cloud technologies.
+4. Contribute to impactful projects that solve real-world problems."
+}
+
+# Call the functions
+who_am_i
+fullname
+bio
+year_in_tech
+dream_job
+goals
