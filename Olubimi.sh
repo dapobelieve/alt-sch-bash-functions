4fd21920bc635556acdec34809929043af987a3c
olubimi <olubimimichael4@gmail.com>
2024-12-08 15:40:40 +0000

    first assignment

---
+++ Olubimi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+who_am_i() {
+  echo "Hello, I am a Cloud Engineer!"
+}
+
+fullname() {
+  echo "Fullname: Olubimi Michael Olumde"
+}
+
+bio() {
+  echo "Bio: A cloud engineer is responsible for designing, implementing, and managing cloud infrastructure and services, working with cloud platforms like AWS, Azure, and Google Cloud to build scalable and secure solutions for businesses."
+}
+
+year_in_tech() {
+  echo "Year in Tech: I have been in tech for 2 years but currently working in a financial institution as an account maintenance officer"
+}
+
+dream_job() {
+  echo "Dream Job: A Cloud Engineer."
+}
