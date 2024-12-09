cca3bfe0d6a9094fb5762bdd74251dede5253531
Ganiu Babatunde <ganiu.babatunde@avonhealthcare.com>
2024-12-09 19:55:25 +0100

    Babs File

---
+++ Babs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+who_am_i() {
+    echo "Hello, I am Babatunde Ganiu Adeola"
+}
+
+Bio() { 
+    echo "I am in the cloud engineering track with reg no: ALT/SOE/024/1222"
+    echo "I am 27 years old, i'm currently working as IT officer at Avon HMO"
+    echo "I work and stay in Lagos, Nigeria"
+    echo "Passionate about Cloud computing and Leadership"
+    echo "I love making friends" 
+}
+
+Years_in_tech() {
+    echo "I've been in tech for one year" 
+}
+
+Dream_job() {
+    echo "I would like to work as Head of IT OR Dir. at NNPC" 
+}
+
+Goals() {
+    echo "I would love to become a professional in IT field"
+    echo "To incoprate Tech sales into my tech life"
+    echo "To develop tech solutions that would help the country"
+}
+
+who_am_i 
+echo
+
+Bio 
+echo
+
+Years_in_tech 
+echo
+
+Dream_job 
+echo
+
+Goals 
+
