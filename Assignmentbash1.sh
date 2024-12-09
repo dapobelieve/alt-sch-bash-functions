2b888289d09a077042dd587192d912e8af62809f
Felix <fndudim@gmail.com>
2024-12-09 18:42:34 +0700

    Assignment1 on bash functions

---
+++ Assignmentbash1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash/
+
+who_am_i() {
+  echo "Hello,I am Felix Ndudim🎉"
+}
+
+#code that prints my personal profile
+
+who_am_i
+
+# echo  "full name: Felix Ndudim"    #full name
+
+
+echo "--"                        #separator
+
+echo "Occupation: Banking"       #occupation
+
+echo "--"
+
+echo "Educational Background: Master's Degree" #qualifications
+
+echo "--" #separator
+
+echo "Skills:Python,Matlab"    # skills 
+
+echo "--"
+
+echo "Interests:Information Technology specialization Cloud Engineering"  #Interests
+
+echo "--"
+
+echo "Goals:Aspiring to become a certified cloud architect and contribute to sustainable computing solutions."
