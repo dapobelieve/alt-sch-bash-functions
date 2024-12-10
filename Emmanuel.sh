0aed5d0a5b8e3036f2ad156440279c34f1ae1727
DonttalKtoemma <emmy.chuks1995@gmail.com>
2024-12-10 15:11:05 -0700

    assignment done

---
+++ Emmanuel.sh
@@ -0,0 +1,20 @@
+fullname() {
+  echo "Hello, I am Emmanuel Chukwuemeka"
+}
+
+Bio() {
+  echo "Continously learning and staying at the forefront of emerging technologies to create impactful solution and commited to building efficient products"
+}
+
+
+Years_in_tech() {
+  echo "1 year and half"
+}
+
+Dream_job() {
+  echo "Software Engineer/Developer"
+}
+
+Goals() {
+  echo "Master a programming language, Learn New Technologies, Cloud computing"
+}
