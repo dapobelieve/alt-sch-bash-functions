3b5b4abf41c74f53d213ae4387bac80560478a58
Fave <onyenekeflavour@gmail.com>
2024-12-12 19:16:29 +0100

    My bash-scripting assignment

---
+++ Fave.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+who_am_i() {
+  echo "Hello, I am Fave💕✨"
+}
+
+fullname() {
+  echo "Fullname: Favour Onyeneke"
+}
+
+bio() {
+  echo "Bio: I am a software engineer with a passion for building scalable and reliable software systems. Software Engineer who wants to contribute and gain more knowledge. 
+             As well eager to collaborate with like-minded people in tech."
+   echo " I enjoy writing about my experiences and sharing insights to help others on their tech journeys."
+}
+
+year_in_tech() {
+  echo "Year in Tech: I have been in tech for 2 years."
+}
+
+dream_job() {
+  echo "Dream Job: DevOps Engineer at Microsoft."
+}
+
+Goals() {
+  echo "Goals: To become a professional devops engineer and to be able to work with a team of great minds."
+}
