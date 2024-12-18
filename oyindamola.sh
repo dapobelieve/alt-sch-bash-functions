4e6d21677794450b76066f760473136167945f24
Oyindamola Boboye <boboyeoyindamola@gmail.com>
2024-12-18 19:13:22 +0100

    my git assignmet

---
+++ oyindamola.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+who_am_i() {
+  echo "Hello, I am a cloud engineering student🎉"
+}
+
+my_name() {
+    echo "My name is Oyindamola Adeola Boboye "
+    echo "Student ID ALT/SOE/024/0537 "
+}
+
+Bio() { 
+    echo "I am a product designer transitioning into a cloud/DevOps engineer."
+}
+
+Years_in_tech() {
+    echo "I've been in tech for three years." 
+}
+
+Dream_job() {
+    echo "My dream job is definitely to work as a cloud engineer at Amazon Web Services (AWS)." 
+}
+
+Goals() {
+    echo "My goal is to become a highly skilled and successful professional in my career"
+    echo "I aim to become a techpreneur"
+}
+
+who_am_i 
+echo
+
+my_name
+echo
+
+Bio 
+echo
+
+Years_in_tech 
+echo
+
+Dream_job 
+echo
+
+Goals 
