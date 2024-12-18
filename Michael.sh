77e2855eb8febaa91617b13a874a57eb76c395d0
Michael <michealononye@gmail.com>
2024-12-18 02:47:15 -0800

    Assignment Done

---
+++ Michael.sh
@@ -0,0 +1,19 @@
+Fullname() {
+  echo "Hello, I am Ononye Michael."
+}
+
+Bio() {
+  echo "Ononye Michael Ebuka a 26 year old from Anambra state, Nigeria, is a medical intern and Medical Laborartory Science graduate from Imo state university. He combines his expertise in healthcare with a passion for technology, currently pursuing cloud engineering at AltSchool. Coming from a family of Eight, he values responsibilty and teamwork. Alongside his professional pursuits, Michael enjoys playing football and is proactive in health."
+}
+
+Years_in_tech() {
+  echo "One Year"
+}
+
+Dream_job() {
+  echo "My dream job is to become a Cloud Solutions Architect."
+}
+ 
+Goals() {
+  echo "Master Cloud fundamentals, Earn Certifications, Specilize in DevOps and contribute to real world projects"
+}
