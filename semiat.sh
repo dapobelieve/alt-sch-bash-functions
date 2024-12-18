51773f369ab9c5a14f67584104200b9a67383a0d
semiat14 <boladalesemiat@gmail.com>
2024-12-18 21:36:42 +0100

    assignment done

---
+++ semiat.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+who_am_i() {
+  echo "Hi, My name Boladale Semiat Olashubomi "
+}
+Bio() { 
+    echo "I am a cloud engineer (in training), and currently learning at ALTSCHOOL Africa."
+    echo "I am 20, also a 4th year student of Biological Sciences at the University of Abuja."
+    echo " With  love for reading fiction and  curiosity about the potential of web 3, I am always seeking new knowledge and ideas."
+   
+}
+
+Years_in_tech() {
+    echo "I have been in tech less than ayear." 
+}
+
+Dream_job() {
+    echo "My dream job would be to work as a devops engineer at Citadel" 
+}
+
+Goals() {
+    echo "My biggest goal is to be a competent and successful devops engineer"
+    echo "And also to be a part of a meaningful and veery successful project/community"
+   
+}
+
+who_am_i 
+Bio 
+Years_in_tech 
+Dream_job 
+Goals  
+
